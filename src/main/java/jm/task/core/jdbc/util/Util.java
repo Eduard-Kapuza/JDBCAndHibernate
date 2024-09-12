c83ebc24dbc9e30d628bfaeda1119e1a8ed755d0
EDUARDKAPUZA <eduardkapuza@gmail.com>
2024-09-12 22:17:40 +0300

    make CRUD on JDBC

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Objects;
+
+import static jm.task.core.jdbc.common.Constant.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            if (Objects.isNull(connection)) {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error get connection");
+            e.printStackTrace();
+            return connection;
+        }
+        return connection;
+    }
 }
